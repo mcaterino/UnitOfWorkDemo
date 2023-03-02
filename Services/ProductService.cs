de3991669f92c2f7d30208e6339fe083581a94b4
Mauricio Caterino <mauricio_caterino@outlook.com>
2023-03-02 14:28:16 -0400

    add product service implementation

--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Interfaces;
+using Core.Models;
 using Services.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -15,6 +16,85 @@ namespace Services
         public ProductService(IUnitOfWork unitOfwork)
         {
             _unitOfwork = unitOfwork;
+        }
+
+        public async Task<bool> CreateProduct(ProductDetails productDetails)
+        {
+            if (productDetails != null)
+            {
+                await _unitOfwork.Product.AddAsync(productDetails);
+
+                var product = _unitOfwork.Save();
+
+                if (product > 0)
+                {
+                    return true;
+                }
+                else return false;
+            }
+            return false;
+        }
+
+        public async Task<bool> DeleteProduct(int id)
+        {
+            if (id > 0)
+            {
+                var product = await _unitOfwork.Product.GetByIdAsync(id);
+                if (product != null)
+                {
+                    _unitOfwork.Product.Delete(product);
+                    var productDeleted = _unitOfwork.Save();
+
+                    if (productDeleted > 0)
+                    {
+                        return true;
+                    }
+                    else return false;
+                }
+            }
+            return false;
+        }
+
+        public async Task<IEnumerable<ProductDetails>> GetAllProducts()
+        {
+            var products = await _unitOfwork.Product.GetAllAsync();
+            return products;
+        }
+
+        public async Task<ProductDetails?> GetProductById(int id)
+        {
+            if (id > 0)
+            {
+                var product = await _unitOfwork.Product.GetByIdAsync(id);
+                if (product != null)
+                {
+                    return product;
+                }
+            }
+            return null;
+        }
+
+        public async Task<bool> UpdateProduct(ProductDetails productDetails)
+        {
+            var product = await _unitOfwork.Product.GetByIdAsync(productDetails.Id);
+            if (product != null)
+            {
+                product.ProductName = productDetails.ProductName;
+                product.ProductDescription = productDetails.ProductDescription;
+                product.ProductPrice = productDetails.ProductPrice;
+                product.ProductStock = productDetails.ProductStock;
+
+                _unitOfwork.Product.Update(product);
+
+                var productUpdated = _unitOfwork.Save();
+
+                if (productUpdated > 0)
+                {
+                    return true;
+                }
+                else return false;
+            }
+            return false;
         }
     }
 }
